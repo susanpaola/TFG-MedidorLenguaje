fa3c0acfa86237026ed3ab17ac7b1c37aac0a1dc
susanpaola <susanpaola.herrera@alu.uclm.es>
2025-03-02 20:13:56 +0100

    actualizaciones de codigo

--- static/script.js
+++ static/script.js
@@ -1,3 +1,5 @@
+let archivoTranscripcion = "";
+
 function subirVideo() {
     let fileInput = document.getElementById("videoFile").files[0];
     let formData = new FormData();
@@ -10,6 +12,14 @@ function subirVideo() {
     .then(response => response.json())
     .then(data => {
         document.getElementById("resultado").innerText = data.transcripcion;
+        archivoTranscripcion = data.archivo;  // Guarda el nombre del archivo para descargar
+
+        // Mostrar botón de descarga
+        document.getElementById("descargarBtn").style.display = "block";
     })
     .catch(error => console.error("Error:", error));
+}
+
+function descargarTranscripcion() {
+    window.location.href = `/descargar/${archivoTranscripcion}`;
 }
