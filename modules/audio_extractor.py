68ceeae925a94e82d8d4b3279bfac7740e8a3476
susanpaola <susanpaola.herrera@alu.uclm.es>
2025-03-02 20:34:57 +0100

    nuevas actualizaciones

--- modules/audio_extractor.py
+++ modules/audio_extractor.py
@@ -1,7 +1,10 @@
-from moviepy.editor import VideoFileClip
+import ffmpeg
 
 class AudioExtractor:
     @staticmethod
     def extraer_audio(video_path: str, audio_path: str):
-        video = VideoFileClip(video_path)
-        video.audio.write_audiofile(audio_path)
+        try:
+            ffmpeg.input(video_path).output(audio_path, format='mp3', acodec='mp3').run(overwrite_output=True)
+            print(f"Audio extraído correctamente: {audio_path}")
+        except Exception as e:
+            print(f"Error al extraer audio: {e}")
