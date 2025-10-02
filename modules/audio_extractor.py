d895e5625218ebc2fa38574eefcd2a2b0a85d786
susanpaola <SusanPaola.Herrera@alu.uclm.es>
2025-10-02 18:29:49 +0200

    Limpiar rama main

--- modules/audio_extractor.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-def extract_audio(video_path, audio_path):
-    command = [
-        "ffmpeg", "-i", video_path, "-vn",
-        "-acodec", "pcm_s16le", "-ar", "16000", "-ac", "1", audio_path, "-y"
-    ]
-    subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
