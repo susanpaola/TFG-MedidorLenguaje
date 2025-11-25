60f7b3f3bfbcdc1772cdee7600b9f8b2a43ff187
susanpaola <SusanPaola.Herrera@alu.uclm.es>
2025-11-25 12:29:49 +0100

    maneja y almacena información relacionada con transcripciones de archivos de audio

---
+++ negocio/entities/transcription.py
@@ -0,0 +1,11 @@
+# negocio/entities/transcription.py
+from dataclasses import dataclass
+from datetime import datetime
+
+@dataclass
+class Transcription:
+    file_original: str
+    file_audio: str
+    transcription_txt: str
+    transcription_pdf: str
+    created_at: datetime = datetime.utcnow()
